5c3e183e6c34629593f1f6e939f0fe9ab51ad7d8
Matias Rivera <mrivera@chroma.agency>
2025-10-22 14:17:43 -0300

    fix: comentar verificación de código de estado 204 en el servicio de productos

--- producteca/products/products.py
+++ producteca/products/products.py
@@ -264,8 +264,8 @@ class ProductService(BaseService):
         response = requests.post(endpoint_url, json=data, headers=headers)
         if not response.ok:
             raise Exception(f"Error getting product {product_variation.sku} - {product_variation.code}\n {response.text}")
-        if response.status_code == 204:
-            raise Exception("Status code is 204, meaning nothing was updated or created")
+        # if response.status_code == 204:
+        #     raise Exception("Status code is 204, meaning nothing was updated or created")
         
         _logger.info(f"response text: {response.text}")
         response_data = response.json()
